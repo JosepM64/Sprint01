09e02994adb4a7ab4d0c5bda637332c1bde12799
JMT <josepmtapia@gmail.com>
2023-06-01 13:10:18 +0200

    Feina Tema04 update ciber casa 01/06

---
+++ Tema04/Exercici01.php
@@ -0,0 +1,21 @@
+<?php 
+// Crea una classe Employee, defineix com a atributs el seu nom i sou. Definir un mètode initialize que rebi com a paràmetres el nom i sou. Plantejar un segon mètode print que imprimeixi el nom i un missatge si ha de pagar o no impostos (si el sou supera 6000, paga impostos).
+
+class Employee {
+var $nom;
+var $sou;
+
+}
+
+function Employee ($nom, $sou) {
+
+$this ->nom=$nom;
+$this ->sou=$sou;
+
+}
+
+//getters -setters
+
+
+
+?>

4a5fa9c2fc0b6c0b93a6f744bd4dcbcf70826c83
JosepM64 <josepmtapia@gmail.com>
2023-06-02 22:40:02 +0200

    pujada 2 de juny

--- Tema04/Exercici01.php
+++ Tema04/Exercici01.php
@@ -1,21 +1,40 @@
 <?php 
 // Crea una classe Employee, defineix com a atributs el seu nom i sou. Definir un mètode initialize que rebi com a paràmetres el nom i sou. Plantejar un segon mètode print que imprimeixi el nom i un missatge si ha de pagar o no impostos (si el sou supera 6000, paga impostos).
 
+
+$employee = new Employee("Josep Maria Tapia", 7000);
+$employee->printEmploye();
+
+
 class Employee {
-var $nom;
-var $sou;
+public $nom;
+public $sou;
+
+
+
+function __construct ($miNom, $miSou) {
+
+$this ->nom=$miNom;
+$this ->sou=$miSou;
 
 }
 
-function Employee ($nom, $sou) {
-
-$this ->nom=$nom;
-$this ->sou=$sou;
+//getters obtenir el sou
+function getSou(){
+return $this->sou;
 
 }
 
-//getters -setters
+// mostrar el sou
+function printEmploye(){
+echo  $this->nom ." tens un sou de ".$this->sou."\n";
+if ($this->sou >6000) {
+        echo "Has de pagar";
 
-
+    } else {
+        echo "No has de pagar";
+    }
+    }  
+}
 
 ?>

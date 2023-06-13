211ec2e5812ef1a388ab92bdd00f636de3bea8e2
JosepM64 <josepmtapia@gmail.com>
2023-06-13 23:12:17 +0200

    Update casa ciber 13/06

--- Tema03/Niv03/Ex01.php
+++ Tema03/Niv03/Ex01.php
@@ -3,16 +3,17 @@
 //Donat un array d’enters, fes un programa que:Retorni cada valor de l’array elevat al cub fent servir la funció array_map().
 
 $z = array(2, 3, 4, 5);
-$y =["j","l","v","a","q"];
+// $y =["j","l","v","a","q"];
 var_dump($z);
-var_dump($y);
+// var_dump($y);
 
-$resultado = array_map(function($z){
-return ($z*3);
+$resultado = array_map(function($valor){
+return pow($valor,3);
 
-},$arrayMap);
+},$z);
 
 //$resultado = array_chunk($y,2);
-echo $resultado;
+print_r($resultado);
 var_dump($resultado);
+
 ?>
---
+++ Tema03/Niv03/Ex02.php
@@ -0,0 +1,20 @@
+<?php 
+
+//Donat un array d’strings, fes un programa que: Retorni un array on només estiguin els strings que tinguin un nom parell de caràcters usant la funció array_filter().
+
+$y =["jamón","lluis","viva","avio","qq"];
+
+// var_dump($y);
+echo strlen($y);
+
+
+// $resultado = array_map(function($valor){
+//return pow($valor,3);
+
+// },$z);
+
+//$resultado = array_chunk($y,2);
+// print_r($resultado);
+// var_dump($resultado);
+
+?>
